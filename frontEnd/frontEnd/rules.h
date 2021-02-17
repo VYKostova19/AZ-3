490b43226e2ad4efba87c7b5de040e3ab6cd8351
Yuki Dimanov <pddimitrova19@codingburgas.bg>
2021-02-17 20:46:42 +0200

    Add design to the menu for the code

---
+++ frontEnd/frontEnd/rules.h
@@ -0,0 +1,29 @@
+#pragma once
+using namespace std;
+
+void rules() {
+	cout << "|-----------------------------------------|" << endl;
+	cout << "|         _   _  _  _     __   __         |" << endl;
+	cout << "|        |_)  |  |  |    |__  |__         |" << endl;
+	cout << "|        | \\  |__|  |__  |__   __|        |" << endl;
+	cout << "|                                         |" << endl;
+	cout << "|                                         |" << endl;
+	cout << "|       --Rules for Hangman Game--        |" << endl;
+	cout << "|                                         |" << endl;
+	cout << "|   1. You must choose one person in the  |" << endl;
+	cout << "|   group who will be the host.           |" << endl;
+	cout << "|   2. The host has to think of a secret  |" << endl;
+	cout << "|   word and write it in the console.     |" << endl;
+	cout << "|   3. The players should start guessing  |" << endl;
+	cout << "|   what the word is by choosing to guess |" << endl;
+	cout << "|   the whole word or just a letter that  |" << endl;
+	cout << "|   may be included in it(the word).      |" << endl;
+	cout << "|   4. If the players guess correctly,    |" << endl;
+	cout << "|   the blanks will be filled with the    |" << endl;
+	cout << "|   correct letters.                      |" << endl;
+	cout << "|   5. The players have 8 chances of      |" << endl;
+	cout << "|   guessing and in the 9th incorrect     |" << endl;
+	cout << "|   attempt they lose the game.           |" << endl;
+	cout << "|                                         |" << endl;
+	cout << "|-----------------------------------------|" << endl;
+}
